32e7c91db83fc363b431ed2f499cf83bbf910ebb
github user name <github email>
2024-02-14 17:55:00 -0500

    Added the option for subtraction

--- src/App.java
+++ src/App.java
@@ -2,16 +2,36 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Enter two whole numbers");
-
         Scanner scan=new Scanner(System.in);
+
+        System.out.println("Enter two whole numbers");
 
         int number1= scan.nextInt();
 
         int number2=scan.nextInt();
 
-        System.out.println(number1+number2);
+        System.out.println("Do you want to add them or subtract them (type subtract or add)");
 
+        String operator= scan.next();
+
+        
+
+        if(operator.equals("add"))
+        {
+            int sum=number1+number2;
+            System.out.println(sum);
+
+        }
+        else if(operator.equals("subtract"))
+        {
+            int difference=number1-number2;
+            System.out.println(difference);
+        }
+        
+
+     
+
+        
 
     }
 }
